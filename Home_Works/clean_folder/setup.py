38cd229bb389c76c71d28a45d13e0a829b33b942
Serhii Korobchenko <jchild2008@gmail.com>
2022-07-18 21:06:55 +0300

    Update setup.py

--- Home_Works/clean_folder/setup.py
+++ Home_Works/clean_folder/setup.py
@@ -4,7 +4,7 @@ setup(
     name='clean_folder',
     version='1',
     description='arrange your folder',
-    url='http://github.com/dummy_user/useful',
+    url='https://github.com/serhii-korobchenko/Python_Core_Module_7/tree/main/Home_Works/clean_folder',
     author='Serhii Korobchenko and GoIT',
     author_email='flyingcircus@example.com',
     license='MIT',
