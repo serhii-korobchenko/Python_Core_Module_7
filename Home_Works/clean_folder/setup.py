3c620e9fe125c9ec498c0b0e03f44dbb9f69b39d
Serhii Korobchenko <jchild2008@gmail.com>
2022-07-18 21:30:47 +0300

    Update 7 module

--- Home_Works/clean_folder/setup.py
+++ Home_Works/clean_folder/setup.py
@@ -8,5 +8,6 @@ setup(
     author='Serhii Korobchenko and GoIT',
     author_email='flyingcircus@example.com',
     license='MIT',
-    packages=find_namespace_packages()
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:search_function']}
 )
--- Lectures/pip_instal packedges.py
+++ Lectures/pip_instal packedges.py
@@ -2,3 +2,4 @@
 # python -m pip install (package name)  --> install some_package
 # python -m pip uninstall (package name)- uninstal some packege
 # pip documentation  - https://pip.pypa.io/en/stable/
+# pip install -e  D:\PYTHON\Python_Core_Module_7\Home_Works\clean_folder
