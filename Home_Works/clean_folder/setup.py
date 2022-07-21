2b52280d49c96bf9e13511de9e8e2e63964a10f0
Serhii Korobchenko <jchild2008@gmail.com>
2022-07-21 23:19:39 +0300

    Update module 7

--- Home_Works/clean_folder/setup.py
+++ Home_Works/clean_folder/setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='clean_folder',
-    version='1',
-    description='arrange your folder',
+    version='0.0.1',
+    description='Arrange your folder',
     url='https://github.com/serhii-korobchenko/Python_Core_Module_7/tree/main/Home_Works/clean_folder',
     author='Serhii Korobchenko and GoIT',
     author_email='flyingcircus@example.com',
     license='MIT',
-
-    entry_points={'console_scripts': ['clean-folder = clean_folder.clean']}
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder=clean_folder.clean']}
 )
+# Packaging Python Projects
--- Lectures/pip_instal packedges.py
+++ Lectures/pip_instal packedges.py
@@ -3,5 +3,11 @@
 # python -m pip uninstall (package name)- uninstal some packege
 # pip documentation  - https://pip.pypa.io/en/stable/
 # pip install -e  D:\PYTHON\Python_Core_Module_7\Home_Works\clean_folder
+# py -m pip install -e D:\PYTHON\Python_Core_Module_7\Home_Works\clean_folder 
 # pip help
 # python -m pip uninstall clean_folder
+
+# py -m pip install --upgrade build
+# py -m build
+# py -m pip install --upgrade twine
+# py -m twine upload --repository testpypi dist/*
